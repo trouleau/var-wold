785e74ec323cd5845d2a66225222bb0f61ef5cbc
trouleau <william.trouleau@epfl.ch>
2020-03-13 16:01:56 +0100

    Updated beta update, not working yet

--- tsvar/wold_model.py
+++ tsvar/wold_model.py
@@ -70,7 +70,7 @@ def _wold_model_init_cache(events):
                         break
                 l -= 1
                 last_idx_tlj[int(j)] = int(l)
-                # Set delta_ijk
+                # Set delta_ikj
                 delta_ikj[i][k,j] = last_tki - events[j][l]
         last_tki = tki
     return delta_ikj, valid_mask_ikj
@@ -89,8 +89,8 @@ class WoldModel(Model):
     `end_time`.
 
     Note: to make the computation of the log-likelihood faster, we pre-compute
-    once the inter-arrival times in attribute `delta_ijk`, where
-    `delta_ijk[i][j,k]` holds $t_{k-1}^i - t_l^j$, where $l$ is the index of
+    once the inter-arrival times in attribute `delta_ikj`, where
+    `delta_ikj[i][k, j]` holds $t_{k-1}^i - t_l^j$, where $l$ is the index of
     the latest event in dimension $j$ such that $t_l^j < t_{k-1}^i$, which is
     used to computed the intensity function for dimension $i$ at time $t_k^i$.
     """
@@ -167,25 +167,44 @@ class WoldModel(Model):
         return log_like
 
 
-@numba.jit(nopython=True)
-def _update_alpha(as_pr, ar_pr, zp_po, D_ikj):
+# @numba.jit(nopython=True)
+def _update_alpha(as_pr, ar_pr, zp_po, bs_po, br_po, last_t):
     dim = as_pr.shape[1]
     as_po = np.zeros_like(as_pr)  # Alpha posterior shape, to return
     ar_po = np.zeros_like(as_pr)  # Alpha posterior rate, to return
     for i in range(dim):
-        as_po[:, i] = (as_pr[:, i] + zp_po[i].sum(axis=0))
-        ar_po[:, i] = (ar_pr[:, i] + D_ikj[i].sum(axis=0))
+        as_po[:, i] = as_pr[:, i] + zp_po[i].sum(axis=0)
+        ar_po[0, i] = ar_pr[0, i]
+        ar_po[1:, i] = ar_pr[1:, i] + (bs_po[:, i] / br_po[:, i]) * last_t[i]
     return as_po, ar_po
 
 
-def _update_z(as_po, ar_po, D_ikj):
-    dim = len(D_ikj)
+# @numba.jit(nopython=True)
+def _update_beta(bs_pr, br_pr, zp_po, as_po, ar_po, delta_ikj, last_t):
+    bs_po = np.zeros_like(bs_pr)  # Alpha posterior shape, to return
+    br_po = np.zeros_like(br_pr)  # Alpha posterior rate, to return
+    dim = as_po.shape[1]
+    for i in range(dim):
+        bs_po[:, i] = bs_pr[:, i] + np.sum(zp_po[i][:, 1:], axis=0)
+        br_po[:, i] = (br_pr[:, i]
+                       + np.sum(zp_po[i][:, 1:] * delta_ikj[i][:, 1:], axis=0)
+                       + (as_po[1:, i] / ar_po[1:, i]) * last_t[i])
+    return bs_po, br_po
+
+
+def _update_z(as_po, ar_po, bs_po, br_po, delta_ikj):
+    dim = as_po.shape[1]
     zp = list()
     for i in range(dim):
-        epi = (digamma(as_po[np.newaxis, :, i])
-               - np.log(ar_po[np.newaxis, :, i])
-               + np.log(D_ikj[i] + 1e-10))
-        # epi -= np.max(epi, axis=1)[:, np.newaxis]
+        # Expected value
+        epi = np.zeros_like(delta_ikj[i])
+        epi += (digamma(as_po[np.newaxis, :, i])
+                - np.log(ar_po[np.newaxis, :, i]))
+        epi[:, 1:] += (np.log(br_po[:, i]) - digamma(bs_po[:, i])
+                       + ((delta_ikj[i][:, 1:] + 1) * br_po[:, i]
+                          / (bs_po[:, i] - 1)))
+        # Softmax
+        epi = epi - epi.max(axis=1)[:, np.newaxis]
         epi = np.exp(epi)
         epi /= epi.sum(axis=1)[:, np.newaxis]
         zp.append(epi)
@@ -197,57 +216,68 @@ class VariationalWoldModel(WoldModel):
     def set_data(self, events, end_time=None):
         super().set_data(events, end_time)
         # TODO: fix this once virtual events in fixed in parent class
-        self.D_ikj = [np.zeros_like(arr) for arr in self.delta_ikj]
         for i in range(self.dim):
             self.events[i] = self.events[i][:-1].numpy()
-            valid_mask_ikj_i = np.hstack((
+            self.valid_mask_ikj[i] = np.hstack((
                 np.ones((self.n_jumps[i]-1, 1)),
                 self.valid_mask_ikj[i][:-1, :].numpy()))
-            delta_ikj_i = np.hstack((
+            self.delta_ikj[i] = np.hstack((
                 np.zeros((self.n_jumps[i]-1, 1)),
                 self.delta_ikj[i][:-1, :].numpy()))
-            dts = np.hstack((self.events[i][0], np.diff(self.events[i])))
-            self.D_ikj[i] = (valid_mask_ikj_i
-                             * dts[:, np.newaxis]
-                             / (1 + delta_ikj_i))
-            self.D_ikj[i][~valid_mask_ikj_i.astype(bool)] = 1e-20
-        # Remove `delta_ikj` and `valid_mask_ikj`, we only need `D_ikj` to fit
-        del self.delta_ikj
-        del self.valid_mask_ikj
+        self.last_t = [self.events[i][-1] for i in range(self.dim)]
+
         # Number of events per dimension
         self.n_jumps = np.array(list(map(len, self.events)))
 
     @enforce_fitted
-    def fit(self, *, as_pr, ar_pr, zc_pr, max_iter=100, tol=1e-5):
+    def fit(self, *, as_pr, ar_pr, bs_pr, br_pr, zc_pr, max_iter=100, tol=1e-5):
         self._as_pr = as_pr  # Alpha prior, shape of Gamma distribution
         self._ar_pr = ar_pr  # Alpha prior, rate of Gamma distribution
+        self._bs_pr = bs_pr  # Beta prior, shape of Gamma distribution
+        self._br_pr = br_pr  # Beta prior, rate of Gamma distribution
         self._zc_pr = zc_pr  # Z prior, concentration
 
+        # shape: (dim+1: j, dim: i)
         self._as_po = self._as_pr.copy()  # Alpha posterior, shape of Gamma distribution
         self._ar_po = self._ar_pr.copy()  # Alpha posterior, rate of Gamma distribution
 
-        self._zp_po = list()
-        for i in range(self.dim):
-            # Z posterior, probabilities of Categorical distribution
-            self._zp_po.append((self._zc_pr[i]
-                                / self._zc_pr[i].sum(axis=1)[:, np.newaxis]))
+        # shape: (dim: j, dim: i)
+        self._bs_po = self._bs_pr.copy()  # Beta posterior, shape of InvGamma distribution
+        self._br_po = self._br_pr.copy()  # Beta posterior, rate of InvGamma distribution
 
-        # print('-'*50, 0)
-        # print('Alpha posterior mean:')
-        # print(np.round(self._as_po / self._ar_po, 2))
-        # print('Z[0] posterior probabilities')
-        # print(self._zp_po[0])
+        # shape: (dim: i, #events_i: k, dim: j)
+        self._zp_po = list()  # Z posterior, probabilities of Categorical distribution
+        for i in range(self.dim):
+            self._zp_po.append(self._zc_pr[i]
+                               / self._zc_pr[i].sum(axis=1)[:, np.newaxis])
+
+        print('-'*50, 0)
+        print('Alpha posterior mean:')
+        print(np.round(self._as_po / self._ar_po, 2))
+        print('Z[0] posterior probabilities')
+        print(self._zp_po[0])
 
         for i in range(max_iter):
-            # print('-'*50, i+1)
+            print('-'*50, i+1)
 
             self._as_po, self._ar_po = _update_alpha(as_pr=self._as_pr,
                                                      ar_pr=self._ar_pr,
                                                      zp_po=self._zp_po,
-                                                     D_ikj=self.D_ikj)
+                                                     bs_po=self._bs_po,
+                                                     br_po=self._br_po,
+                                                     last_t=self.last_t)
 
-            # print('Alpha posterior mean:')
-            # print(np.round(self._as_po / self._ar_po, 2))
+            self._bs_po, self._br_po = _update_beta(bs_pr=self._bs_pr,
+                                                    br_pr=self._br_pr,
+                                                    zp_po=self._zp_po,
+                                                    as_po=self._as_po,
+                                                    ar_po=self._ar_po,
+                                                    delta_ikj=self.delta_ikj,
+                                                    last_t=self.last_t)
+
+            print('Alpha posterior mean:')
+            print(np.round(self._as_po / self._ar_po, 2))
+
             if self._as_po.min() < 0:
                 raise RuntimeError("Negative alpha shape!")
             if self._as_po.min() < 0:
@@ -255,7 +285,9 @@ class VariationalWoldModel(WoldModel):
 
             self._zp_po = _update_z(as_po=self._as_po,
                                     ar_po=self._ar_po,
-                                    D_ikj=self.D_ikj)
+                                    bs_po=self._bs_po,
+                                    br_po=self._br_po,
+                                    delta_ikj=self.delta_ikj)
 
-            # print('Z posterior probabilities')
-            # print(self._zp_po[0])
+            print('Z posterior probabilities')
+            print(self._zp_po[0])

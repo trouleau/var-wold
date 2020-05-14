9bba718eaada590f594f2bab847e5ceaa29abf75
trouleau <william.trouleau@epfl.ch>
2020-05-14 11:52:16 +0200

    Fixed setup

--- setup.py
+++ setup.py
@@ -71,7 +71,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["numpy", "torch"],
+    install_requires=[
+        "certifi==2019.11.28",
+        "numpy==1.18.1",
+        "torch==1.3.1",
+        "scipy==1.4.1",
+        "numba==0.47.0",
+        "pandas==0.25.3",
+        "matplotlib==3.1.2"
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,

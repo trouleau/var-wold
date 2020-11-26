dbc2fc3d55c5f078afcebfdbac58754791e2c677
trouleau <william.trouleau@epfl.ch>
2020-11-26 19:09:03 +0100

    seeded data regime script

--- script_make_experiment_data_regime.sh
+++ script_make_experiment_data_regime.sh
@@ -1,4 +1,4 @@
 for n in {499,753,1134,1709,2576,3881,5848,8810,13274,20000}
 do
-    python script_make_job.py -e output/dataRegimes-n10-2/ -d 10 -n $n -g 5 -s 4
+    python script_make_job.py -e output/dataRegimes-n10-3/ -d 10 -n $n -g 5 -s 4 --seed 545693892
 done
